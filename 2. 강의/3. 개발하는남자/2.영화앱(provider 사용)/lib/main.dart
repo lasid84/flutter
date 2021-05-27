ec2b89b1ebd290c847952662e65b1a840ffdfe38
DESKTOP-LCVGKJI\N19086 <lasid84@gmail.com>
2021-05-27 13:53:54 +0900

    2021.05.27 개발하는남자 강의 중 영화앱 커밋

---
+++ 2. 강의/3. 개발하는남자/2.영화앱(provider 사용)/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_provider_sample/src/provider/bottom_navigation_provider.dart';
+import 'package:flutter_provider_sample/src/provider/counter_provider.dart';
+import 'package:flutter_provider_sample/src/provider/movie_provider.dart';
+import 'package:flutter_provider_sample/src/ui/home.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+              create: (BuildContext context) => CounterProvider()),
+          ChangeNotifierProvider(
+              create: (BuildContext context) => MovieProvider()),
+          ChangeNotifierProvider(
+              create: (BuildContext context) => ButtomNavigationProvider()),
+        ],
+        child: Home(),
+      ),
+      // home: ChangeNotifierProvider(
+      //   create: (BuildContext context) => CounterProvider(),
+      //   child: Home(),
+      // ),
+    );
+  }
+}
