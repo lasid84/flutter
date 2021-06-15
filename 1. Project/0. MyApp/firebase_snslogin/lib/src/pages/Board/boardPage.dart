17eb913d7957cefc0037df3cbd0742208359728f
lasid84 <80510446+lasid84@users.noreply.github.com>
2021-06-15 22:46:17 +0900

    2021.06.15 22:46 게시판 타일 만드는 중

--- 1. Project/0. MyApp/firebase_snslogin/lib/src/pages/Board/boardPage.dart
+++ 1. Project/0. MyApp/firebase_snslogin/lib/src/pages/Board/boardPage.dart
@@ -3,42 +3,63 @@ import 'package:firebase_snslogin/src/pages/Board/boardModel.dart';
 import 'package:firebase_snslogin/src/pages/Board/firebase.dart';
 import 'package:flutter/material.dart';
 
-class BoardPage extends StatelessWidget {
-  // final boardRef = FirebaseFirestore.instance.collection(Firebase.com_document_id).withConverter<Board>(
-  //   fromFirestore: (snapshot, _) => Board.fromJson(snapshot.data()!),
-  //   toFirestore: (board, _) => board.toJson(),
-  // );
-
-  final Stream<QuerySnapshot> boardRef = FirebaseFirestore.instance
-      .collection(Firebase.com_document_id)
-      .orderBy("documentID", descending: true)
-      .limit(20)
-      .snapshots();
+class BoardPage extends StatefulWidget {
+  const BoardPage({Key? key}) : super(key: key);
 
   @override
+  _BoardPageState createState() => _BoardPageState();
+}
+
+class _BoardPageState extends State<BoardPage> {
+  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
+      .collection(Firebase.com_document_id)
+      .orderBy('boardID', descending: true)
+      .snapshots();
+  @override
   Widget build(BuildContext context) {
-    print('test0');
     return StreamBuilder<QuerySnapshot>(
-      stream: boardRef,
+      stream: _usersStream,
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.hasError) {
           return Text('Something went wrong');
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          print('test1');
           return Text("Loading");
         }
 
-        return new ListView(
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
-            print('test123');
-            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-            return new ListTile(
-              title: Text('test'), //new Text(data['documentID']),
-              subtitle: Text('test'), //new Text(data['contents']),
-            );
-          }).toList(),
+        return Scaffold(
+          body: new ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data =
+                  document.data() as Map<String, dynamic>;
+              return Card(
+                child: new ListTile(
+                  // trailing: Icon(
+                  //   Icons.favorite,
+                  //   color: Colors.grey,
+                  // ),
+                  leading: FlutterLogo(
+                    size: 56.0,
+                  ),
+                  title: new Text(
+                    data['title'],
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  subtitle: Row(
+                    children: [
+                      Text(
+                        data['contents'],
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                  isThreeLine: true,
+                ),
+              );
+            }).toList(),
+          ),
         );
       },
     );
